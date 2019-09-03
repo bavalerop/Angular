ea97472ff6aaa8e38ce99119c50ce4b5af265cd6
Brayan Valero <bvalero@cltech-int.net>
2019-09-03 11:12:35 -0500

    Plantilla1 para proyectos

--- prueba/src/app/Components/Fruta/Fruta.component.ts
+++ prueba/src/app/Components/Fruta/Fruta.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    selector: 'fruta',
-    templateUrl: ''
+    selector: 'app-fruta',
+    templateUrl: './Fruta.component.html',
+    styleUrls: ['./Fruta.component.css']
 })
+export class FrutaComponent {
+    title = 'prueba titulo';
+}
 
