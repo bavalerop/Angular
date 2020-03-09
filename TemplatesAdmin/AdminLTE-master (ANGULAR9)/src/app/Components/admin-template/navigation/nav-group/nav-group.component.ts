bc43a707fe4216788dce93a3e9f11ef12660c74a
Brayan Valero <bvalero@cltech-int.net>
2020-03-09 13:35:04 -0500

    Cambios en menu dinamico brayan

--- TemplatesAdmin/AdminLTE-master (ANGULAR9)/src/app/Components/admin-template/navigation/nav-group/nav-group.component.ts
+++ TemplatesAdmin/AdminLTE-master (ANGULAR9)/src/app/Components/admin-template/navigation/nav-group/nav-group.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input, NgZone, OnInit} from '@angular/core';
-import {Location} from '@angular/common';
 import { NavigationModel } from '../../../../Models/Navigations/Navigation.model';
 
 @Component({
@@ -9,38 +8,12 @@ import { NavigationModel } from '../../../../Models/Navigations/Navigation.model
 })
 export class NavGroupComponent implements OnInit {
   @Input() item: NavigationModel;
-  @Input() activeId: any;
-  public gradientConfig: any;
 
-  constructor(private zone: NgZone, private location: Location) {
+  constructor() {
   }
 
   ngOnInit() {
-    // at reload time active and trigger link
-    let currentUrl = this.location.path();
-    // tslint:disable-next-line: no-string-literal
-    if (this.location['_baseHref']) {
-      // tslint:disable-next-line: no-string-literal
-      currentUrl = this.location['_baseHref'] + this.location.path();
-    }
-    // tslint:disable-next-line: quotemark
-    const link = "a.nav-link[ href='" + currentUrl + "' ]";
-    const ele = document.querySelector(link);
-    if (ele !== null && ele !== undefined) {
-      const parent = ele.parentElement;
-      const upParent = parent.parentElement.parentElement;
-      const lastParent = upParent.parentElement;
-      if (parent.classList.contains('pcoded-hasmenu')) {
-          parent.classList.add('pcoded-trigger');
-          parent.classList.add('active');
-      } else if (upParent.classList.contains('pcoded-hasmenu')) {
-          upParent.classList.add('pcoded-trigger');
-          upParent.classList.add('active');
-      } else if (lastParent.classList.contains('pcoded-hasmenu')) {
-          lastParent.classList.add('pcoded-trigger');
-          lastParent.classList.add('active');
-      }
-    }
+
   }
 
 }
